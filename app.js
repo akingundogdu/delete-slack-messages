905a4106864cead3f1d91daa3911647739a16e62
Akin Gundogdu <akin-gundogdu@hotmail.com>
2022-12-07 20:31:55 +0300

    trying to fetch 1000 messages from slack api to delete them

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const {WebClient} = require('@slack/web-api');
 
+const token = 'your_slack_app_user_auth_token';
+const channelId = 'your_channel_id_that_you_want_to_delete';
+const myUserId = 'your_user_id';
+
 console.log('Getting started to clear your slack messages');
 
 const deleteSlackMessages = async () => {
@@ -8,6 +12,18 @@ const deleteSlackMessages = async () => {
 
 
 (async () => {
+    //trying to fetch 1000 messages from slack api to delete them
+    const result = await client.conversations.history({
+        channel: channelId,
+        limit: 1000,
+        // latest: '1618827164.002200',
+    });
+    console.log('result.messages', result.messages.length);
+    const list = result.messages.map((message) => {
+        return message;
+    });
+    console.log('list', list.length);
+
     await deleteSlackMessages();
     console.log('done!');
 })();
