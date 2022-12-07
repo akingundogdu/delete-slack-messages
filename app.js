b98c45005dd4986c833ffbc1aefe61126dbd0b8e
Akin Gundogdu <akin-gundogdu@hotmail.com>
2022-12-07 20:42:16 +0300

    implement slack delete api and works fine

--- app.js
+++ app.js
@@ -4,13 +4,34 @@ const token = 'your_slack_app_user_auth_token';
 const channelId = 'your_channel_id_that_you_want_to_delete';
 const myUserId = 'your_user_id';
 const slackClient = new WebClient(token);
+
 console.log('Getting started to clear your slack messages');
 
-const deleteSlackMessages = async () => {
-    console.log('deleted slack message xxxx ');
+const sleep = (ms = 1500) => {
+    return new Promise((r) => {
+        return setTimeout(r, ms);
+    });
 };
 
-const getMessagesFromSlack = async ({ slackClient, channelId, myUserId }) => {
+const deleteSlackMessages = async (messages) => {
+    for (const index in messages) {
+        const message = messages[index];
+        try {
+            if (message.user === myUserId) {
+                await sleep();
+                await slackClient.chat.delete({
+                    channel: channelId,
+                    ts: message.ts,
+                });
+                console.log('the slack message deleted ', message.ts);
+            }
+        } catch (error) {
+            console.log('there is a problem to delete processing', error.data.error);
+        }
+    }
+};
+
+const getMessagesFromSlack = async ({slackClient, channelId, myUserId}) => {
     const conversationHistories = await slackClient.conversations.history({
         channel: channelId,
         limit: 1000,
@@ -27,7 +48,21 @@ const getMessagesFromSlack = async ({ slackClient, channelId, myUserId }) => {
 
 
 (async () => {
-    const messages = await getMessagesFromSlack({ slackClient, channelId, myUserId })
-    await deleteSlackMessages(messages);
+    try {
+        let deletedMessagesCount = 0;
+        do {
+            console.log(deletedMessagesCount > 0 ? 'clean process continue...' : '');
+
+            const messages = await getMessagesFromSlack({slackClient, channelId, myUserId})
+
+            console.log('slack messages count', messages.length);
+
+            await deleteSlackMessages(messages);
+
+            deletedMessagesCount = messages.length;
+        } while (deletedMessagesCount > 0);
+    } catch (error) {
+        console.error(error);
+    }
     console.log('done!');
 })();
