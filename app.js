37c2c857a7ae761140391ddaffa6c2774fb42799
Akin Gundogdu <akin-gundogdu@hotmail.com>
2022-12-07 20:37:53 +0300

    code review completed everything works fine!

--- app.js
+++ app.js
@@ -3,27 +3,31 @@ const {WebClient} = require('@slack/web-api');
 const token = 'your_slack_app_user_auth_token';
 const channelId = 'your_channel_id_that_you_want_to_delete';
 const myUserId = 'your_user_id';
-
+const slackClient = new WebClient(token);
 console.log('Getting started to clear your slack messages');
 
 const deleteSlackMessages = async () => {
     console.log('deleted slack message xxxx ');
 };
 
-
-(async () => {
-    //trying to fetch 1000 messages from slack api to delete them
-    const result = await client.conversations.history({
+const getMessagesFromSlack = async ({ slackClient, channelId, myUserId }) => {
+    const conversationHistories = await slackClient.conversations.history({
         channel: channelId,
         limit: 1000,
         // latest: '1618827164.002200',
     });
-    console.log('result.messages', result.messages.length);
-    const list = result.messages.map((message) => {
-        return message;
+    console.log('conversationHistories.messages.length', conversationHistories.messages.length);
+    const list = conversationHistories.messages.map((message) => {
+        return message.user === myUserId;
     });
-    console.log('list', list.length);
+    console.log('my.conversationHistories.messages.length', list.length);
 
-    await deleteSlackMessages();
+    return list;
+}
+
+
+(async () => {
+    const messages = await getMessagesFromSlack({ slackClient, channelId, myUserId })
+    await deleteSlackMessages(messages);
     console.log('done!');
 })();
