ca33b1577b274d7334d5256ff096ba4dea1c6e74
Özgür Ersoy <oersoy@outlook.com.tr>
2022-12-08 15:47:09 +0100

    Delete replies in thread

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const {WebClient} = require('@slack/web-api');
+const { WebClient } = require('@slack/web-api');
 
 const token = 'your_slack_app_user_auth_token';
 const channelId = 'your_channel_id_that_you_want_to_delete';
@@ -8,61 +8,87 @@ const slackClient = new WebClient(token);
 console.log('Getting started to clear your slack messages');
 
 const sleep = (ms = 1500) => {
-    return new Promise((r) => {
-        return setTimeout(r, ms);
-    });
+  return new Promise((r) => {
+    return setTimeout(r, ms);
+  });
+};
+
+const deleteReplies = async ({ ts }) => {
+  const replies = await slackClient.conversations.replies({
+    channel: channelId,
+    limit: 1000,
+    ts,
+  });
+
+  if (replies.messages.length > 0) {
+    console.log(`Reply count for ${ts}`, replies.messages.length);
+
+    for await (let reply of replies.messages) {
+      try {
+        if (reply.user === myUserId) {
+          await sleep();
+
+          await slackClient.chat.delete({ channel: channelId, ts: reply.ts });
+        }
+      } catch (error) {
+        console.log('There was a problem when deleting reply message', error?.data?.error || error.message);
+      }
+    }
+  }
+
+  return Promise.resolve(replies);
 };
 
 const deleteSlackMessages = async (messages) => {
-    for (const index in messages) {
-        const message = messages[index];
-        try {
-            if (message.user === myUserId) {
-                await sleep();
-                await slackClient.chat.delete({
-                    channel: channelId,
-                    ts: message.ts,
-                });
-                console.log('the slack message deleted ', message.ts);
-            }
-        } catch (error) {
-            console.log('there is a problem to delete processing', error.data.error);
-        }
+  for await (let message of messages) {
+    try {
+      await deleteReplies({ ts: message.ts });
+
+      if (message.user === myUserId) {
+        await sleep();
+        await slackClient.chat.delete({ channel: channelId, ts: message.ts });
+
+        console.log('The slack message deleted ', message.ts);
+      }
+    } catch (error) {
+      console.log('There was a problem to delete processing', error?.data?.error || error.message);
     }
+  }
 };
 
-const getMessagesFromSlack = async ({slackClient, channelId, myUserId}) => {
-    const conversationHistories = await slackClient.conversations.history({
-        channel: channelId,
-        limit: 1000,
-        // latest: '1618827164.002200',
-    });
-    console.log('conversationHistories.messages.length', conversationHistories.messages.length);
-    const list = conversationHistories.messages.map((message) => {
-        return message.user === myUserId;
-    });
-    console.log('my.conversationHistories.messages.length', list.length);
+const getConversationHistoriesFromSlack = async ({ latest = null }) => {
+  const conversationHistories = await slackClient.conversations.history({
+    channel: channelId,
+    limit: 1,
+    latest,
+  });
 
-    return list;
+  return Promise.resolve({
+    messages: conversationHistories.messages,
+    hasMore: conversationHistories.has_more,
+    latest: conversationHistories.messages[conversationHistories.messages.length - 1].ts,
+  });
 }
 
 
 (async () => {
-    try {
-        let deletedMessagesCount = 0;//after completing the delete process for all slack messages, this will stop.
-        do {
-            console.log(deletedMessagesCount > 0 ? 'clean process continue...' : '');
+  try {
+    let latest = null;
+    let hasMore = true; //after completing the delete process for all slack messages, this will stop.
 
-            const messages = await getMessagesFromSlack({slackClient, channelId, myUserId})
+    do {
+      console.log(latest ? 'Clean process continue...' : '');
 
-            console.log('slack messages count', messages.length);
+      const conversation = await getConversationHistoriesFromSlack({ latest });
+      console.log('Slack message count', conversation.messages.length);
 
-            await deleteSlackMessages(messages);
+      latest = conversation.latest;
+      hasMore = conversation.hasMore;
+      await deleteSlackMessages(conversation.messages);
 
-            deletedMessagesCount = messages.length;
-        } while (deletedMessagesCount > 0);
-    } catch (error) {
-        console.error(error);
-    }
-    console.log('done!');
+    } while (hasMore);
+  } catch (error) {
+    console.error(error);
+  }
+  console.log('Done!');
 })();
